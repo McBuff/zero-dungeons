54e67ae77e441c47e13ee597a2f10a3bd9902ec9
unknown <bouckaert.r@gmail.com>
2020-04-20 20:52:48 +0200

    renamed folder, will test first

---
+++ canvasTest/Index.js
@@ -0,0 +1,11 @@
+// Generate 
+let varCrap
+
+//simple rando nr gen
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+// Test van var
+console.log(objTown.Name);
+
