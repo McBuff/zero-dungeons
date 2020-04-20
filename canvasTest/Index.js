ebb5b50416669e615e0fc59d96bfc7e256c12677
unknown <bouckaert.r@gmail.com>
2020-04-20 21:28:36 +0200

    canvastest

--- canvasTest/Index.js
+++ canvasTest/Index.js
@@ -1,13 +1,29 @@
 // Generate 
-let varCrap
+let objCanvas = {
+  source:document.getElementById("myCanvas1"),
+  context: objCanvas.getContext("2d"),
+};
+
+// let Vancwidth = canvas.width;
+// let height = canvas.height;
+// let i;
+
+for (i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+
+}
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+
+
+
 
 //simple rando nr gen
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
-
-
-
 // Test van var
-console.log(objTown.Name);
+//console.log(objTown.Name);
 
