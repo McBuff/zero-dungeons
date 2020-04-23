e5c4614d16a92318bbdfa94631ad2c938fbfed85
Gertjan Van den Broek <sir.mcbuff@gmail.com>
2020-04-23 20:52:35 +0200

    folled around with classes

--- diceroller/diceroller.js
+++ diceroller/diceroller.js
@@ -1,5 +1,52 @@
+//import * as die from die.js;
+
+
+
+function GenerateGuid()
+{
+    var sGuid = "";
+    for (var i = 0; i< 32; i++)
+    {
+        sGuid += Math.floor(Math.random() * 0xF).toString(0xF);
+    };
+    return sGuid;
+}
+
+class die{
+    constructor(sides, result =  -1){
+        self.sides = sides;
+        self.result = result;
+        self.guid = GenerateGuid();
+
+    }
+
+    roll() {
+        console.log('Die rolled with GUID:'+ guid);
+        
+    }
+
+    // _createguid(){
+    //     var sGuid = "";
+    //     for (var i = 0; i< 32; i++)
+    //     {
+    //         sGuid += Math.floor(Math.random() * 0xF).toString(0xF);
+    //     }
+    //     return sGuid;
+    // }
+    
+};
+
+
 function handleDiceRoll(){
     
+    for (let i = 0; i < 20; ++i) {
+        var d  = new die( 20);
+        console.log('die '+ (i+1) );
+        d.roll();        
+    };
+    
+
+
     // user has clicked button    
     
     // Retreive user selected dice
@@ -31,6 +78,7 @@ function handleDiceRoll(){
     var elem = document.getElementById('log');
     // elem.append(elem);
     
+    
     console.log("rolled dice, result = X");
 }
 
@@ -41,7 +89,7 @@ function rollDice(){
     for (i = 0; i < arguments.length; i++)
     {
         console.log("Rolling dice '" + arguments[i]+ "'");
-    }
+    };
 }
 
 function clearlog(){
--- diceroller/die.js
+++ diceroller/die.js
@@ -1,3 +1,32 @@
 // create dice class here:
 // required: sides
 // required: result
+
+
+//import * as GUID from guid.js;
+
+export class  die{
+    constructor(sides, result =  -1){
+        self.sides = sides;
+        self.result = result;
+        self.guid = GUID._createguid();
+
+    }
+
+    roll() {
+        console.log("Die rolled with GUID: ${guid}");
+
+        
+    }
+
+    // _createguid(){
+    //     var sGuid = "";
+    //     for (var i = 0; i< 32; i++)
+    //     {
+    //         sGuid += Math.floor(Math.random() * 0xF).toString(0xF);
+    //     }
+    //     return sGuid;
+    // }
+    
+}
+
